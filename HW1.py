6707a48262b7d42f630867df148f4a2929e2ae5d
@andrey.rozhdestvin <andrey.rozhdestvin@sbermarket.ru>
2023-05-31 09:43:03 +0300

    learnQA

--- HW1.py
+++ HW1.py
@@ -1,8 +1,6 @@
 from json.decoder import JSONDecodeError
 import requests
 
-#payload = {"name":"Andrey"}
-#response1 = requests.get("https://playground.learnqa.ru/api/hello", params={"name":"Andrey"})
 response = requests.get("https://playground.learnqa.ru/api/get_text")
 
 print(response.text)        #выводим текстовый ответ
@@ -10,4 +8,5 @@ try:
     parsed_response_text = response.json()
     print(parsed_response_text['answer'])          # пробуем распарсить ответ если он json
 except JSONDecodeError:
-    print("Response is not a JSON format")          # если отрабатывает JSONDecodeError, то выводим ошибку
+    #print("Response is not a JSON format")      # если отрабатывает JSONDecodeError, то выводим ошибку
+    print(response.text)
